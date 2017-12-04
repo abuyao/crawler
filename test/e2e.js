b42b887b107b60b8cf0d41846a840170dcbf0a57
姚熊 <yaoxiong@yaoxiongdeMacBook-Pro.local>
2017-12-04 09:31:11 +0800

    【修改】修改代码风格

--- test/e2e.js
+++ test/e2e.js
@@ -1,48 +1,98 @@
-var webdriver = require('selenium-webdriver'),
+const webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     Key = webdriver.Key,
     until = webdriver.until;
 
-var driver = new webdriver.Builder()
-    .forBrowser('firefox')//firefox //phantomjs
-    .build();
-
-// let home = driver.findElement(By.tagName("div"));
-// home.then(data=>{
-//     console.log('taobao',data.getText());
-// });
-async function getlistname() {
-    try {
-        await driver.sleep(1000);
-        var el = await driver.findElement(By.id("mainsrp-itemlist")).findElements(By.className("item"));
-        await el.map(async (item, index) => {
-            let href = await item.findElement(By.className("title")).findElement(By.tagName("a")).getAttribute("href");
-            console.log('detail href ' + index, href);
-            return href;
+class TAOBAOCRAWLER {
+    constructor(url){
+        this.url = url;
+        this.driver = null;
+        this.init();
+    }
+    init(){
+        this.initBuilder();
+        this.initURL();
+    }
+    async serach(key) {
+        return this.driver.findElement(By.xpath("//*[@class='search-combobox-input']")).sendKeys(key, Key.RETURN);
+    }
+    async getHrefText() {
+        let items = await this.getItems();
+        this.driver.findElement(items[0]).getTagName().then(result=>{
+            console.log('result', result);
+        }, err=>{
+            console.log('yaoerr', err);
         });
-        // el.forEach(async href=>{
-        //     await driver.navigate().to(href);
-        //     await driver.sleep(2000);
-        //     await driver.navigate().back();
-        //     await driver.sleep(2000);
+        let link = await this.getLink(items[0]);
+        console.log(link);
+
+        // let list = items.map(async (item)=>{
+        //     try{
+        //         // console.log('item', item);
+        //         let i = await this.getLink(item);
+        //         // console.log('i', i);
+        //         return i ;
+        //     }catch(err){
+        //         // console.log('err', err);
+        //     }
+        //     // return this.gethref(this.getLink(item));
         // });
-    } catch(err){
-        getlistname();
+        // return list;
+    }
+    async gethref(item) {
+        if(!item) return '';
+        console.log('item', item);
+        let href = item.getAttribute("href");
+        return href
+    }
+    async getLink(item) {
+        item.then(result =>{
+            console.log('result', result);
+        });
+        try{
+            let cl = await item.getTagName();
+            console.log('yaoxiongyu item.findElement', cl);
+            var title = await item.findElement(By.className("title"));
+            console.log('title', title);
+            var a = await title.findElement(By.tagName("a"));
+            console.log('a', a);
+            var href = await a.getAttribute("href");
+            console.log('href', href);
+
+        }catch(err){console.log('yaoxiong error', err);}
+        let link = await item.findElement(By.className("title")).findElement(By.tagName("a")).getAttribute("href");
+        return link;
+        // return item.findElement(By.xpath("//*[@class='title']//a[0]"));
+    }
+    async getItems() {
+        return this.driver.findElement(By.id("mainsrp-itemlist")).findElements(By.className("item"));
+        // return this.driver.findElements(By.xpath("//*[@id='mainsrp-itemlist']//*[@class='item']"));
+    }
+    sleep(time) {
+        this.driver.sleep(time);
+    }
+    initURL(){
+        this.driver.get('https://www.taobao.com/');
+    }
+    initBuilder(){
+        this.driver = new webdriver.Builder()
+        .forBrowser('firefox')//firefox //phantomjs
+        .build();
+    }
+    quit(){
+        this.driver.quit();
     }
 }
-async function getlist() {
-    await driver.get('https://www.taobao.com/');
-    await driver.findElement(By.className("search-combobox-input")).sendKeys('钢', Key.RETURN);
-    getlistname();
-}
-
-getlist();
-// .then(data=>{
-//     console.log("items", data.findElement(By.className("title")).findElement(By.tagName("a")));
-// });
-driver.getTitle().then((data) => {
-    console.log(data);
+const crawler = new TAOBAOCRAWLER('https://www.taobao.com/');
+crawler.serach("钢");
+crawler.sleep(4000);
+crawler.getHrefText().then((list)=>{
+    list.forEach(function(item){
+        item.then(result=>{
+            // console.log('result', result);
+        }, err=>{
+            // console.log('err', err);
+        });
+    });
 });
-
-driver.sleep(1000 * 100);
-driver.quit();
+crawler.quit();
